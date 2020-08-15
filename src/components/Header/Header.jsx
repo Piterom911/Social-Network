d17f8e458370f54944d23449466f94c01997f1c8
piter <piterom911@gmail.com>
2020-08-16 00:51:08 +0600

    17.Стилизация header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import style from "./Header.module.scss"
+import s from "./Header.module.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
-        <header className={style.header}>
-            <div className={`container ${style.container_header}`}>
-                <a href="#main" className={style.logo}>
-                    <img src="./logo-header.png" alt=""/>
+        <header className={s.header}>
+            <div className={`container ${s.container_header}`}>
+                <a href="#main" className={s.logo}>
+                    <img className={s.logoImg} src="https://themified.com/friend-finder/images/logo.png" alt=""/>
                 </a>
-                <nav>
-                    <ul>
-                        <li><a href="#notNow">Something 1</a></li>
-                        <li><a href="#notNow">Something 2</a></li>
-                        <li><a href="#notNow">Something 3</a></li>
-                        <li><a href="#notNow">Something 4</a></li>
+                <nav className={s.nav}>
+                    <form className={s.search}>
+                        <div className={s.formGroup}>
+                            <FontAwesomeIcon className={s.icon} icon={faSearch} />
+                            <input type="text" className={s.formControl} placeholder="Search friends, photos, videos"/>
+                        </div>
+                    </form>
+                    <ul className={s.list}>
+                        <li className={s.item}><a className={s.link} href="#notNow">Something 1</a></li>
+                        <li className={s.item}><a className={s.link} href="#notNow">Something 2</a></li>
+                        <li className={s.item}><a className={s.link} href="#notNow">Something 3</a></li>
+                        <li className={s.item}><a className={s.link} href="#notNow">Something 4</a></li>
                     </ul>
                 </nav>
             </div>
