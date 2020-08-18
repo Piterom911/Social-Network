05cf2128f824626a1026afed755f83d13e4f87fb
piter <piterom911@gmail.com>
2020-08-18 17:08:10 +0600

    24/25 Refactoring Map -- messages, posts

--- src/components/Content/Messages/ContactList/ContactList.jsx
+++ src/components/Content/Messages/ContactList/ContactList.jsx
@@ -3,14 +3,57 @@ import s from "./ContactList.module.scss";
 import Contact from "./Contact/Contact";
 
 const ContactList = () => {
+
+    const contacs = [
+        {
+            id: 1,
+            name: "Linda Lohan",
+            time: "a min ago",
+            message: "Oh... Do not be afraid.",
+            ava: "https://themified.com/friend-finder/images/users/user-2.jpg"
+        },
+        {
+            id: 2,
+            name: "Julia Cox",
+            time: "an hour ago",
+            message: "see you soon",
+            ava: "https://themified.com/friend-finder/images/users/user-10.jpg"
+        },
+        {
+            id: 3,
+            name: "Sophia Lee",
+            time: "13 hour ago",
+            message: "Okay fine. thank you",
+            ava: "https://themified.com/friend-finder/images/users/user-3.jpg"
+        },
+        {
+            id: 4,
+            name: "John Doe",
+            time: "Yesterday",
+            message: "hey anybody there",
+            ava: "https://themified.com/friend-finder/images/users/user-4.jpg"
+        },
+        {
+            id: 5,
+            name: "Anna Young",
+            time: "2 days ago",
+            message: "I gotta go",
+            ava: "https://themified.com/friend-finder/images/users/user-9.jpg"
+        },
+        {
+            id: 6,
+            name: "Richard Bell",
+            time: "2 days ago",
+            message: "Hey there",
+            ava: "https://themified.com/friend-finder/images/users/user-8.jpg"
+        },
+    ];
+
+    const contact = contacs.map( data => <Contact id={data.id} name={data.name} time={data.time} message={data.message} ava={data.ava}/>)
+
     return (
         <div className={s.contacts}>
-            <Contact id={"1"} name={"Linda Lohan"} time={"a min ago"} message={"Oh... Do not be afraid."} ava={"https://themified.com/friend-finder/images/users/user-2.jpg"}/>
-            <Contact id={"2"} name={"Julia Cox"} time={"an hour ago"} message={"see you soon"} ava={"https://themified.com/friend-finder/images/users/user-10.jpg"}/>
-            <Contact id={"3"} name={"Sophia Lee"} time={"13 hour ago"} message={"Okay fine. thank you"} ava={"https://themified.com/friend-finder/images/users/user-3.jpg"}/>
-            <Contact id={"4"} name={"John Doe"} time={"Yesterday"} message={"hey anybody there"} ava={"https://themified.com/friend-finder/images/users/user-4.jpg"}/>
-            <Contact id={"5"} name={"Anna Young"} time={"2 days ago"} message={"I gotta go"} ava={"https://themified.com/friend-finder/images/users/user-9.jpg"}/>
-            <Contact id={"6"} name={"Richard Bell"} time={"2 days ago"} message={"Hey there?"} ava={"https://themified.com/friend-finder/images/users/user-8.jpg"}/>
+            { contact }
         </div>
     )
 };
--- src/components/Content/Messages/Dialog/Dialog.jsx
+++ src/components/Content/Messages/Dialog/Dialog.jsx
@@ -3,12 +3,42 @@ import s from "./Dialog.module.scss";
 import Message from "./Message/Message";
 
 const Dialog = () => {
+
+    const messages = [
+        {
+            ava: "https://themified.com/friend-finder/images/users/user-2.jpg",
+            me: "notMe",
+            name: "Linda Lohan",
+            time: "3 days ago",
+            message: "Hello my little friend!"
+        },
+        {
+            ava: "https://themified.com/friend-finder/images/users/user-1.jpg",
+            me: "me",
+            name: "Sarah Cruiz",
+            time: "3 days ago",
+            message: "I really want you to know about..."
+        },
+        {
+            ava: "https://themified.com/friend-finder/images/users/user-2.jpg",
+            me: "notMe",
+            name: "Linda Lohan",
+            time: "3 days ago",
+            message: "You are scaring me! Please say what you want to say!"
+        },
+        {
+            ava: "https://themified.com/friend-finder/images/users/user-1.jpg",
+            me: "me",
+            name: "Sarah Cruiz",
+            time: "3 days ago",
+            message: "Oh... Do not be afraid. It is no matter any more. You will know soon)"
+        }
+    ];
+
+    const message = messages.map( data => <Message ava={data.ava} me={data.me} name={data.name} date={data.time} message={data.message}/>);
     return (
         <div className={s.dialog}>
-            <Message ava={"https://themified.com/friend-finder/images/users/user-2.jpg"} me={"notMe"} name={"Linda Lohan"} date={"3 days ago"} message={'Hello my little friend!'}/><br/>
-            <Message ava={"https://themified.com/friend-finder/images/users/user-1.jpg"} me={"me"} name={"Sarah Cruiz"} date={"3 days ago"} message={'I really want you to know about...'}/><br/>
-            <Message ava={"https://themified.com/friend-finder/images/users/user-2.jpg"} me={"notMe"} name={"Linda Lohan"} date={"3 days ago"} message={'You are scaring me! Please say what you want to say!'}/><br/>
-            <Message ava={"https://themified.com/friend-finder/images/users/user-1.jpg"} me={"me"} name={"Sarah Cruiz"} date={"3 days ago"} message={'Oh... Do not be afraid. It is no matter any more. You will know soon)'}/><br/>
+            { message }
         </div>
     )
 };
